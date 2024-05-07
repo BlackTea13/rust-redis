1793f6100c4bd50ee128b4dd276903316bd5e350
BlackTea13 <robertblum2001@gmail.com>
2024-05-07 21:04:49 +0700

    feat: parser done

--- src/frame.rs
+++ src/frame.rs
@@ -1,6 +1,7 @@
 use bytes::{Buf, Bytes};
+use std::convert::TryInto;
 use std::fmt;
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 use std::num::TryFromIntError;
 use std::string::FromUtf8Error;
 
@@ -24,14 +25,92 @@ impl Frame {
     // checks if the message can be decoded
     pub fn check(src: &mut Cursor<&[u8]>) -> Result<(), Error> {
         match get_u8(src)? {
-            b'+' => Ok(()),
-            b'-' => Ok(()),
-            _ => Ok(()),
+            b'+' => {
+                get_line(src)?;
+                Ok(())
+            }
+            b'-' => {
+                get_line(src)?;
+                Ok(())
+            }
+            b':' => {
+                let len = get_decimal(src)?;
+                Ok(())
+            }
+            b'$' => {
+                if b'-' == peek_u8(src)? {
+                    skip(src, 4)
+                } else {
+                    let len: usize = get_decimal(src)?.try_into()?;
+
+                    skip(src, len + 2) // skip \r\n too
+                }
+            }
+            b'*' => {
+                let len = get_decimal(src)?;
+
+                for _ in 0..len {
+                    Frame::check(src)?;
+                }
+
+                Ok(())
+            }
+            other => Err(format!("protocol error; invalid frame type `{}`", other).into()),
         }
     }
 
     pub fn parse(src: &mut Cursor<&[u8]>) -> Result<Frame, Error> {
-        unimplemented!();
+        match get_u8(src)? {
+            b'+' => {
+                let line = get_line(src)?.to_vec();
+                let string = String::from_utf8(line)?;
+                Ok(Frame::Simple(string))
+            }
+            b'-' => {
+                let line = get_line(src)?.to_vec();
+                let string = String::from_utf8(line)?;
+                Ok(Frame::Error(string))
+            }
+            b':' => {
+                let decimal = get_decimal(src)?;
+                Ok(Frame::Integer(decimal))
+            }
+            b'$' => {
+                if b'-' == peek_u8(src)? {
+                    let line = get_line(src)?;
+
+                    if line != b"-1" {
+                        return Err("protocol errorl invalid frame formar".into());
+                    }
+
+                    Ok(Frame::Null)
+                } else {
+                    let len = get_decimal(src)?.try_into()?;
+                    let n = len + 2;
+
+                    if src.remaining() < n {
+                        return Err(Error::Incomplete);
+                    }
+
+                    let data = Bytes::copy_from_slice(&src.chunk()[..len]);
+
+                    skip(src, n)?;
+
+                    Ok(Frame::Bulk(data))
+                }
+            }
+            b'*' => {
+                let len = get_decimal(src)?.try_into()?;
+                let mut out = Vec::with_capacity(len);
+
+                for _ in 0..len {
+                    out.push(Frame::parse(src)?);
+                }
+
+                Ok(Frame::Array(out))
+            }
+            other => Err(format!("protocol error; invalid frame type `{}`", other).into()),
+        }
     }
 }
 
@@ -41,6 +120,15 @@ fn get_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
     }
 
     Ok(src.get_u8())
+}
+
+fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
+    if src.remaining() < n {
+        return Err(Error::Incomplete);
+    }
+
+    src.advance(n);
+    Ok(())
 }
 
 fn get_line<'a>(src: &'a mut Cursor<&[u8]>) -> Result<&'a [u8], Error> {
@@ -56,6 +144,20 @@ fn get_line<'a>(src: &'a mut Cursor<&[u8]>) -> Result<&'a [u8], Error> {
     }
 
     Err(Error::Incomplete)
+}
+
+fn peek_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+    Ok(src.get_ref()[0])
+}
+
+fn get_decimal(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
+    use atoi::atoi;
+    let line = get_line(src)?;
+
+    atoi::<u64>(line).ok_or_else(|| "protocol error, invalid frame format".into())
 }
 
 impl From<String> for Error {
